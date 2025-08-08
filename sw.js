97d00cfe53317cff9ca6c77656517c41657c057d
Tina210qv <zuyayatinaishe@gmail.com>
2025-08-08 16:21:56 +0100

    offline page

---
+++ sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'offline-v1';
+const OFFLINE_URL = 'offline-page.html';
+const ASSETS_TO_CACHE = [
+  OFFLINE_URL,
+  'images/EmiraTech-MW-Logo-.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      await cache.addAll(ASSETS_TO_CACHE);
+    })()
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    (async () => {
+      if ('navigationPreload' in self.registration) {
+        await self.registration.navigationPreload.enable();
+      }
+    })()
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.mode === 'navigate') {
+    event.respondWith(
+      (async () => {
+        try {
+          const preloadResponse = await event.preloadResponse;
+          if (preloadResponse) {
+            return preloadResponse;
+          }
+
+          const networkResponse = await fetch(event.request);
+          return networkResponse;
+        } catch (error) {
+          console.log('Fetch failed; returning offline page instead.', error);
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+        }
+      })()
+    );
+  }
+});
