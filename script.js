c8df722f059105dd5f64563acb02845d068b114b
Tina210qv <zuyayatinaishe@gmail.com>
2025-08-08 03:37:14 +0100

    your message

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('.nav-menu ul');
+
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+        menuToggle.classList.toggle('active');
+    });
+});
